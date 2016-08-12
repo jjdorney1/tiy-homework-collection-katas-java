c2cc1ebd30c056823366445d8412af509f5efd5e
jeffreydorney <jjdorney1@gmail.com>
2016-08-12 16:25:07 -0400

    created file in intellij

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+/**
+ * Created by jeffreydorney on 8/12/16.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Collection Katas for Java\n");
+    }
+}
