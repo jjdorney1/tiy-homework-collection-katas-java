45b8a4920fb263d41dcf03cff8b004840aaab12c
jeffreydorney <jjdorney1@gmail.com>
2016-08-16 14:50:38 -0400

    completed all arraylist katas and successfully formatted them for printing

--- src/Kata.java
+++ src/Kata.java
@@ -5,7 +5,13 @@ import java.util.ArrayList;
  */
 public class Kata {
 
+    //
+    // ARRAYLIST KATAS
+    //
+
     public static boolean firstLast6(ArrayList<Integer> numbers) {
+
+        // checks to see that either the first or last number are equal to zero
         if( (numbers.get(0) == 6) || (numbers.get(numbers.size()-1) == 6) ) {
             return true;
 
@@ -15,6 +21,8 @@ public class Kata {
     }
 
     public static boolean sameFirstLast(ArrayList<Integer> numbers) {
+
+        // checks to see if the array size is bigger than 1 and the first and last are equal
         if( (numbers.size() > 1) && (numbers.get(0) == numbers.get(numbers.size()-1)) ) {
             return true;
 
@@ -24,6 +32,8 @@ public class Kata {
     }
 
     public static ArrayList rotateLeft3(ArrayList<Integer> numbers) {
+
+        // created a new array which gets assigned shifted one over from the original array
         ArrayList<Integer> newNumbers = new ArrayList<>();
         newNumbers.add(numbers.get(1));
         newNumbers.add(numbers.get(2));
@@ -32,4 +42,36 @@ public class Kata {
 
     }
 
+    public static ArrayList reverse3(ArrayList<Integer> numbers) {
+
+        // created a new array which reverses the order of the original
+        ArrayList<Integer> reverse3Nums = new ArrayList<>();
+        reverse3Nums.add(numbers.get(2));
+        reverse3Nums.add(numbers.get(1));
+        reverse3Nums.add(numbers.get(0));
+        return reverse3Nums;
+
+    }
+
+    public static int sumFirstPenultimate(ArrayList<Integer> numbers) {
+
+        // checks to see if the array size is zero
+        if(numbers.size() == 0) {
+            return 0;
+
+            // checks to see if the array size is less than two numbers
+        } else if(numbers.size() < 2) {
+            return numbers.get(0) + numbers.get(1);
+
+        } else {
+            // returns the first number and the second from last number added together
+            return numbers.get(0) + numbers.get(numbers.size() - 2);
+        }
+    }
+
+    //
+    // HASHMAP KATAS
+    //
+
+    //public static
 }
--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,13 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("\nCollection Katas (Java)\n************************");
+        System.out.println("\nCollection Katas (Java)");
+        System.out.println("\nArrayList Kata Problems\n************************");
+
+        //
+        // ARRAYLIST EXAMPLES
+        //
+
         boolean result; // initial declaration of result
         ArrayList<Integer> numbers = new ArrayList<>(); // initial numbers array
         ArrayList<Integer> numbersOutput = new ArrayList<>(); // initial numbersOutput array
@@ -14,7 +20,7 @@ public class Main {
         // first time values are added
         numbers.add(1);
         numbers.add(2);
-        numbers.add(3);
+        numbers.add(6);
 
         // runs the firstLast6 kata
         result = Kata.firstLast6(numbers);
@@ -37,20 +43,17 @@ public class Main {
         // prints the method and result
         System.out.printf("Kata.firstLast6(%s) -> %s\n", numbers, result);
 
-        // sets numbers back to an empty array
+        // sets numbers back to an empty array etc
         numbers = new ArrayList<>();
 
-        // adds values back into numbers
         numbers.add(13);
         numbers.add(6);
         numbers.add(1);
         numbers.add(2);
         numbers.add(3);
 
-        // runs the firstLast6 kata
         result = Kata.firstLast6(numbers);
 
-        // prints the method and result
         System.out.printf("Kata.firstLast6(%s) -> %s\n", numbers, result);
         System.out.println(""); // new line
 
@@ -80,7 +83,7 @@ public class Main {
         System.out.printf("Kata.sameFirstLast(%s) -> %s\n", numbers, result);
         System.out.println(""); // new line
 
-
+        // first rotate3 example
         numbers = new ArrayList<>();
         numbers.add(1);
         numbers.add(2);
@@ -88,6 +91,7 @@ public class Main {
         numbersOutput = Kata.rotateLeft3(numbers);
         System.out.printf("Kata.rotateLeft3(%s) -> %s\n", numbers, numbersOutput);
 
+        // second rotate3 example
         numbers = new ArrayList<>();
         numbers.add(5);
         numbers.add(11);
@@ -95,15 +99,79 @@ public class Main {
         numbersOutput = Kata.rotateLeft3(numbers);
         System.out.printf("Kata.rotateLeft3(%s) -> %s\n", numbers, numbersOutput);
 
+        // third rotate3 example
         numbers = new ArrayList<>();
         numbers.add(7);
         numbers.add(0);
         numbers.add(0);
         numbersOutput = Kata.rotateLeft3(numbers);
         System.out.printf("Kata.rotateLeft3(%s) -> %s\n", numbers, numbersOutput);
+        System.out.println(""); // new line
 
+        // first reverse3 example
+        numbers = new ArrayList<>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+        numbersOutput = Kata.reverse3(numbers);
+        System.out.printf("Kata.reverse3(%s) -> %s\n", numbers, numbersOutput);
 
+        // second reverse3 example
+        numbers = new ArrayList<>();
+        numbers.add(5);
+        numbers.add(11);
+        numbers.add(9);
+        numbersOutput = Kata.reverse3(numbers);
+        System.out.printf("Kata.reverse3(%s) -> %s\n", numbers, numbersOutput);
 
+        // third reverse3 example
+        numbers = new ArrayList<>();
+        numbers.add(7);
+        numbers.add(0);
+        numbers.add(0);
+        numbersOutput = Kata.reverse3(numbers);
+        System.out.printf("Kata.reverse3(%s) -> %s\n", numbers, numbersOutput);
+        System.out.println(""); // new line
+
+        // first sumFirstPenultimate example
+        numbers = new ArrayList<>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+        int resultInt = Kata.sumFirstPenultimate(numbers);
+        System.out.printf("Kata.sumFirstPenultimate(%s) -> %s\n", numbers, resultInt);
+
+        // second sumFirstPenultimate example
+        numbers = new ArrayList<>();
+        numbers.add(1);
+        numbers.add(1);
+        resultInt = Kata.sumFirstPenultimate(numbers);
+        System.out.printf("Kata.sumFirstPenultimate(%s) -> %s\n", numbers, resultInt);
+
+        // third sumFirstPenultimate example
+        numbers = new ArrayList<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(1);
+        resultInt = Kata.sumFirstPenultimate(numbers);
+        System.out.printf("Kata.sumFirstPenultimate(%s) -> %s\n", numbers, resultInt);
+
+        // forth sumFirstPenultimate example
+        numbers = new ArrayList<>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+        numbers.add(4);
+        resultInt = Kata.sumFirstPenultimate(numbers);
+        System.out.printf("Kata.sumFirstPenultimate(%s) -> %s\n", numbers, resultInt);
+        System.out.println(""); // new line
+
+        //
+        // HASHMAP EXAMPLES
+        //
+
+        System.out.println("\nHashMap Kata Problems\n************************");
 
 
 
